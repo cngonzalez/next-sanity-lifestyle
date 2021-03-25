bc18cc053dd10d936fc580e62cd9318b798c44bc
Carolina Gonzalez <carolina.nicole.gonzalez@gmail.com>
2021-03-24 23:03:46 -0700

    last go before prepping for starter

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -18,6 +18,8 @@ function App({Component, pageProps, router}: AppProps) {
               name="viewport"
               content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
             />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
           </Head>
         <Box>
           <Component {...pageProps} />
